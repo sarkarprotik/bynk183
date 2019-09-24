2d2433a2256e5f55181bf5bd74d5b0e98c5dadd9
Protik Sarkar <sarkar.protik1@gmail.com>
2019-09-24 02:17:00 +0200

    Updates

---
+++ precache-manifest.8f4a25f087ac212499065c062cafe61a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3da74207b77c38d24f8e9e779abbd8f",
+    "url": "/bynk183/index.html"
+  },
+  {
+    "revision": "0729199b1cec4f351cfa",
+    "url": "/bynk183/static/css/2.0a962954.chunk.css"
+  },
+  {
+    "revision": "3a3a59f03ae15646b400",
+    "url": "/bynk183/static/css/main.e3599382.chunk.css"
+  },
+  {
+    "revision": "0729199b1cec4f351cfa",
+    "url": "/bynk183/static/js/2.84372195.chunk.js"
+  },
+  {
+    "revision": "3a3a59f03ae15646b400",
+    "url": "/bynk183/static/js/main.0784a016.chunk.js"
+  },
+  {
+    "revision": "6eeac0d6998ff3e3a301",
+    "url": "/bynk183/static/js/runtime~main.ddb2587b.js"
+  },
+  {
+    "revision": "b6d1b5886a63f03de0663ea314c07f03",
+    "url": "/bynk183/static/media/background.b6d1b588.jpg"
+  },
+  {
+    "revision": "8c4c945f9147506ebae5bdd46379ffd7",
+    "url": "/bynk183/static/media/bynk.8c4c945f.jpg"
+  },
+  {
+    "revision": "45af7c0f65f783d263065d7aab31fd44",
+    "url": "/bynk183/static/media/code.45af7c0f.svg"
+  }
+]);
